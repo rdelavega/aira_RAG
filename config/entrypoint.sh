88a7cfe291b48a5c7cfb8acd60b749f75e60c155
rdelavega <rdlvg711@gmail.com>
2026-05-02 17:06:56 -0600

    fix: entrypoint modified on docker-compose

--- config/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-mkdir -p /root/.openclaw/agents/main/agent
-mkdir -p /root/.openclaw/skills
-mkdir -p /root/.openclaw/workspace
-cp /tmp/openclaw.json /root/.openclaw/openclaw.json
-cp -r /tmp/skills/. /root/.openclaw/skills/
-cp /tmp/agent/auth-profiles.json /root/.openclaw/agents/main/agent/auth-profiles.json
-exec node dist/index.js gateway
